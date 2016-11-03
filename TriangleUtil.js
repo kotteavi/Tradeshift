fa9f5df602367e25b85cc3fee7f1a456c2426bf6
Avi kotte <kotteavin@hotmail.com>
2016-11-02 22:08:14 -0700

    comment and clean up code

--- TriangleUtil.js
+++ TriangleUtil.js
@@ -2,100 +2,98 @@
  * Created by Avi on 11/2/16.
  */
 
-
 /**
- Triangle Challenge:
- Write a program that will determine the type of a triangle.
- It should take the lengths of the triangle's three sides as input,
- and return whether the triangle is equilateral, isosceles or scalene.
- We are looking for solutions that showcase problem solving skills and
- structural considerations that can be applied to larger and potentially
- more complex problem domains. Pay special attention to tests, readability
- of code and error cases.
-
- The resulting code will be used as the basis for broader discussions during the interview.
- Therefore you should be prepared to discuss your choices. Please push your code into GitHub and share the link with me.
-
- */
-
-/**
- * Assumptions
- * Input:
- * Output:
- * Constraints:
- */
-
-/**
- *
- *
+ *  Utility class for triangle shapes
  */
 var TriangleUtil = {
 
-    types: {
-        "invalid": 0,
-        "equilateral": 1,
-        "isosceles": 2,
-        "scalene": 3
+    TYPES: {
+        INVALID: "invalid",
+        EQUILATERAL: "equilateral",
+        ISOSCELES: "isosceles",
+        SCALENE: "scalene"
     },
 
-    // look into the diff types of tirangles
     /**
      * Get triangle type by length of sides.
      *
      * @param {number} a: length of side 1
      * @param {number} b: length of side 2
      * @param {number} c: length of side 3
-     * @returns {number}
+     * @returns {String} @see TYPES
      */
     getType: function (a, b, c) {
 
-        // valid assuming if it is public facing AP
-        // what if it is an API design??
-        // TODO we need better return for errors
-        if (arguments.length < 3 || arguments.length > 4) {
-            return this.types.invalid;
+        // need all sides to calculate type
+        if (arguments.length < 3 || arguments.length > 3) {
+            return this.TYPES.INVALID;
         }
 
         // all inputs must be numbers
         for (var i in arguments) {
-            if (typeof arguments[i] != "number")
-                return this.types.invalid;
+            if (typeof arguments[i] != "number") {
+                return this.TYPES.INVALID;
+            }
         }
 
         // The Triangle Inequality Theorem
         // tests for the validity of the given sides as it relates to a valid triangle
         var isValid = a + b > c && b + c > a && a + c > c;
         if (!isValid) {
-            return this.types.invalid;
+            return this.TYPES.INVALID;
         }
 
+        // all sides of equal length
         if (a == b == c) {
-            return this.types.equilateral;
+            return this.TYPES.EQUILATERAL;
         }
 
+        // two sides of equal length and one unequal side
         if (a == b || a == c || b == c) {
-            return this.types.isosceles;
+            return this.TYPES.ISOSCELES;
         }
 
-        if (a != b != c) {
-            return this.types.scalene;
-        }
+        return this.TYPES.SCALENE;
 
     },
 
+    /**
+     *  Check for equilateral triangle by length of sides
+     *
+     * @param {number} a: length of side 1
+     * @param {number} b: length of side 2
+     * @param {number} c: length of side 3
+     * @returns {boolean} true if an equilateral triangle
+     *  otherwise false
+     */
     isEquilateral: function (a, b, c) {
-        var type = this.getType(a, b, c);
-        return type == this.types.equilateral ? true : false;
+        return this.getType(a, b, c) == this.TYPES.EQUILATERAL;
     },
 
+    /**
+     *  Check for isosceles triangle by length of sides
+     *
+     * @param {number} a: length of side 1
+     * @param {number} b: length of side 2
+     * @param {number} c: length of side 3
+     * @returns {boolean} true if an isosceles triangle
+     *  otherwise false
+     */
     isIsosceles: function (a, b, c) {
-        var type = this.getType(a, b, c);
-        return type == this.types.isosceles ? true : false;
+        return this.getType(a, b, c) == this.TYPES.ISOSCELES;
     },
 
+    /**
+     *  Check for scalene triangle by length of sides
+     *
+     * @param {number} a: length of side 1
+     * @param {number} b: length of side 2
+     * @param {number} c: length of side 3
+     * @returns {boolean} true if an scalene triangle
+     *  otherwise false
+     */
     isScalene: function (a, b, c) {
-        var type = this.getType(a, b, c);
-        return type == this.types.scalene ? true : false;
+        return this.getType(a, b, c) == this.TYPES.SCALENE;
     }
 };
 
@@ -109,50 +107,41 @@ var TriangleUtil = {
 console.log('Test for triangle types');
 var testCount = [0, 0];
 
-assert(testCount, 'handles Equilateral triangle', function () {
-    var test = TriangleUtil.isEquilateral(1, 1, 1);
-    return test;
+assert(testCount, 'handles equilateral triangle', function () {
+    return TriangleUtil.isEquilateral(1, 1, 1);
 });
 
 assert(testCount, 'handles Isosceles triangle', function () {
-    var test = TriangleUtil.isIsosceles(1, 2, 1);
-    return test;
+    return TriangleUtil.isIsosceles(1, 2, 1);
 });
 
 assert(testCount, 'handles Scalene triangle', function () {
-    var test = TriangleUtil.isScalene(1, 4, 3);
-    return test;
+    return TriangleUtil.isScalene(1, 4, 3);
 });
 
 assert(testCount, 'handles invalid triangle lengths', function () {
-    var test = TriangleUtil.getType(1, 4, 50);
-    return test === TriangleUtil.types.invalid;
+    return TriangleUtil.getType(1, 4, 50) == TriangleUtil.TYPES.INVALID;
 });
 
 
 assert(testCount, 'handles invalid input, negative values', function () {
-    var test = TriangleUtil.getType(-1, -4, -3);
-    return test === TriangleUtil.types.invalid;
+    return TriangleUtil.getType(-1, -4, -3) === TriangleUtil.TYPES.INVALID;
 });
 
 assert(testCount, 'handles invalid input, wrong data type', function () {
-    var test = TriangleUtil.getType(1, 5, "x");
-    return test === TriangleUtil.types.invalid;
+    return TriangleUtil.getType(1, 5, "x") == TriangleUtil.TYPES.INVALID;
 });
 
 assert(testCount, 'handles invalid input, no input', function () {
-    var test = TriangleUtil.getType();
-    return test === TriangleUtil.types.invalid;
+    return TriangleUtil.getType() == TriangleUtil.TYPES.INVALID;
 });
 
 assert(testCount, 'handles invalid input, exclude an input', function () {
-    var test = TriangleUtil.getType(2, 4);
-    return test === TriangleUtil.types.invalid;
+    return TriangleUtil.getType(2, 4) == TriangleUtil.TYPES.INVALID;
 });
 
 assert(testCount, 'handles invalid input, include extra inputs', function () {
-    var test = TriangleUtil.getType(2, 4, 7, 8);
-    return test === TriangleUtil.types.invalid;
+    return TriangleUtil.getType(2, 4, 7, 8) == TriangleUtil.TYPES.INVALID;
 });
 
 console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
